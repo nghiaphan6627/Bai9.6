7dd885e0a7bafd9e43320442697796e1fc2a5a29
nghiaphan6627 <phannghia6607@gmail.com>
2026-05-04 21:21:09 +0700

    Khoi tao du an Bai9.6

---
+++ src/test/java/SimpleTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SimpleTest {
+    @Test
+    void demoTest() {
+        assertTrue(true); // Test này luôn đúng, chỉ để Maven có cái để chạy
+    }
+}
